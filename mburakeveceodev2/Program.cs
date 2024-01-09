e9410c05622f9feeadad7e0cc2de55a5b68cc4cc
Muhammmed Burak <148449185+mburakevece@users.noreply.github.com>
2024-01-09 14:02:22 +0300

    Update Program.cs

--- mburakeveceodev2/Program.cs
+++ mburakeveceodev2/Program.cs
@@ -47,7 +47,7 @@
 
                 for (int i = 0; i < numbers.Length; i++)
                 {
-               
+                  Console.WriteLine("the numbers you want are here");
                     Console.WriteLine(numbers[i]);
                 }
 
