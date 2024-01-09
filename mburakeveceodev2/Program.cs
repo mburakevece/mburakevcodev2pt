d77e989433aab799ea8f00e6d846e29e8680cfc1
mburakevece <mburakevece@gmail.com>
2024-01-09 13:06:18 +0300

    Proje dosyası ekle.

---
+++ mburakeveceodev2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace mburakeveceodev2
+{
+    internal class Program
+    {
+      
+        
+            static void Main(string[] args)
+            {
+                Random rnd = new Random();
+                Console.WriteLine("How Many Number Do You Want?");
+                byte count = byte.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Starting Number! ");
+                byte start = byte.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter an Ending Number!");
+                byte end = byte.Parse(Console.ReadLine());
+
+                int[] numbers = new int[count];
+                int num;
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    num = rnd.Next(start, end);
+                    if (i == 0)
+                    {
+                        numbers[i] = num;
+                    }
+
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (numbers[j] == num)
+                        {
+
+                            i--;
+
+                            break;
+                        }
+                        else
+                        {
+                            numbers[i] = num;
+                        }
+                    }
+                }
+
+                Array.Sort(numbers);
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                Console.WriteLine("the numbers you want are here");
+                    Console.WriteLine(numbers[i]);
+                }
+
+            
+        }
+    }
+}
